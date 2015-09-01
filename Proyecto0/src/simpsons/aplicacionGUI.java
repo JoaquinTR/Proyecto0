36b3c58a9ce59413f4f2fd5280a8eef9a5983e6c
Joaquin <joaquintricerri@gmail.com>
2015-09-01 19:36:51 -0300

    Actualizado, temporal.

--- Proyecto0/src/simpsons/aplicacionGUI.java
+++ Proyecto0/src/simpsons/aplicacionGUI.java
@@ -11,11 +11,22 @@ import javax.swing.JButton;
 import javax.swing.ImageIcon;
 import java.awt.Color;
 import javax.swing.SwingConstants;
+import java.awt.SystemColor;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.Label;
+import javax.swing.border.BevelBorder;
+import java.awt.Font;
+import java.awt.TextField;
+import javax.swing.JTextField;
 
 public class aplicacionGUI extends JFrame {
 
 	private JPanel contentPane;
-
+	private JTextField txt;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -36,22 +47,38 @@ public class aplicacionGUI extends JFrame {
 	 * Create the frame.
 	 */
 	public aplicacionGUI() {
+		setTitle("App");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.WHITE);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBorder(new BevelBorder(BevelBorder.LOWERED, SystemColor.activeCaptionBorder, SystemColor.activeCaptionBorder, SystemColor.activeCaptionBorder, SystemColor.activeCaptionBorder));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JLabel img = new JLabel("");
 		img.setVerticalAlignment(SwingConstants.TOP);
 		img.setIcon(new ImageIcon("C:\\Users\\Mica\\Desktop\\index.jpg"));
-		img.setBounds(0, 0, 202, 225);
+		img.setBounds(5, 5, 202, 225);
 		contentPane.add(img);
 		
 		JButton btnHablar = new JButton("hablar");
+		btnHablar.setFont(new Font("OCR A Extended", Font.PLAIN, 15));
+		btnHablar.setBackground(SystemColor.windowBorder);
+		btnHablar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String s="\"DOH!\" -Homero.";
+				txt.setText(s);
+			}
+		});
 		btnHablar.setBounds(222, 34, 89, 23);
 		contentPane.add(btnHablar);
+		
+		txt = new JTextField();
+		txt.setHorizontalAlignment(SwingConstants.CENTER);
+		txt.setEditable(false);
+		txt.setBounds(217, 97, 207, 31);
+		contentPane.add(txt);
+		txt.setColumns(10);
 	}
 }
